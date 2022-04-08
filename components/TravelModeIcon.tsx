95891e36295d241e4e2d845214d4141aa69a3968
Piotrek Brzeziński <thewildtree@outlook.com>
2022-04-08 22:38:09 +0200

    WIP: Add home screen elements

---
+++ components/TravelModeIcon.tsx
@@ -0,0 +1,14 @@
+interface TravelModeIconProps {
+  travel_mode: string;
+}
+
+export default function TravelModeIcon (props: TravelModeIconProps) {
+  let iconName = "local_airport";
+  return (
+    <>
+      <span className="h-14 w-14 rounded-full material-icons-outlined">
+        {iconName}
+      </span>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -35,21 +35,21 @@ const Blog = () => {
 
   return (
     <Layout user={session.user as User}>
-      <header className="bg-white shadow">
-        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">
-            Witaj {session.user.name}!
-          </h1>
-          <h3 className="text-gray-900">
-            Twoje saldo skarbonki: {userData.data && userData.data.user.balance}
-            zł
-          </h3>
+      <header className="bg-white">
+        <div className="flex justify-between mx-auto py-6 px-4 items-center max-w-screen-xl">
+          <div className="max-w-7xl mx-autosm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-800"><span className="font-normal">Witaj</span> {session.user.name}!</h1>
+            <h3 className="text-gray-600">Twoje saldo: <span className="font-semibold">{userData.data ? userData.data.user.balance : 0}zł</span></h3>
+          </div>
+          <div className="flex-shrink-0">
+            <img className="h-14 w-14 rounded-full" src={session.user.image} alt=""/>
+          </div>
         </div>
       </header>
-      <main className="bg-gray-200 shadow">
-        <div className="bg-gray-100 max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 border-t border-gray-200">
-          <div className="layout">
-          </div>
+      <main className="bg-white shadow">
+        <div className="max-w-7xl mx-4 space-y-4 py-4 sm:px-8 lg:px-8">
+          <DestinationCard destination_name="Bukowińska 26C" destination_id={1} />
+          <DestinationCard destination_name="Politechnika Warszawska" destination_id={2} />
         </div>
       </main>
     </Layout>
