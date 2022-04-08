aecddb1d7bfb3d5b53c476c1be8bca68adae34e2
Piotrek Brzeziński <thewildtree@outlook.com>
2022-04-08 18:07:43 +0200

    PWA setup (not working yet)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html className="h-full bg-gray-200">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#333333" />
+      </Head>
       <body className="h-full">
         <Main />
         <NextScript />
