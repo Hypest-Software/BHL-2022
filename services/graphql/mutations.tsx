729e1f62b5b3cbc7ca26b2b270bb6fa4daad1526
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2022-04-09 06:43:19 +0200

    WIP

--- services/graphql/mutations.tsx
+++ services/graphql/mutations.tsx
@@ -1,28 +1,24 @@
 import gql from "graphql-tag";
 
-export const TopUpBalanceMutation = gql`
-  mutation TopUpBalanceMutation($id: String!, $amount: Float!) {
-    topUpBalance(id: $id, amount: $amount) {
-
 export const UpdateBalanceMutation = gql`
-  mutation UpdateBalanceMutation($id: String!, $amount: Float!) {
-    updateBalance(id: $id, amount: $amount) {
-      id
-      balance
+    mutation UpdateBalanceMutation($id: String!, $amount: Float!) {
+        updateBalance(id: $id, amount: $amount) {
+            id
+            balance
+        }
     }
-  }
 `;
 
 export const TransactionCreateMutation = gql`
-  mutation TransactionCreateMutation(
-    $type: TransactionType!
-    $userId: String!
-    $amount: Float!
-  ) {
-    createTransaction(type: $type, userId: $userId, amount: $amount) {
-      id
-      type
-      amount
+    mutation TransactionCreateMutation(
+        $type: TransactionType!
+        $userId: String!
+        $amount: Float!
+    ) {
+        createTransaction(type: $type, userId: $userId, amount: $amount) {
+            id
+            type
+            amount
+        }
     }
-  }
 `;
