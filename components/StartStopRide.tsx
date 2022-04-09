1bad9c88c2d0464b9359dfdccb91f47b5ff0a016
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2022-04-09 05:46:00 +0200

    WIP

--- components/StartStopRide.tsx
+++ components/StartStopRide.tsx
@@ -1,43 +1,83 @@
 import React from "react";
-import { ActiveRideQuery } from "../services/graphql/queries";
-import { useQuery } from "@apollo/client";
+import {ActiveRideQuery, EndRideMutation, StartRideMutation} from "../services/graphql/queries";
+import {useMutation, useQuery} from "@apollo/client";
 import Loading from "./Loading";
 
 interface StartStopRideProps {
-  userId: string;
+    userId: string;
 }
 
 export default function StartStopRide(props: StartStopRideProps) {
-  const activeRide = useQuery(ActiveRideQuery, {
-    variables: {
-      userId: props.userId,
-    },
-  });
+    const activeRide = useQuery(ActiveRideQuery, {
+        variables: {
+            userId: props.userId,
+        },
+    });
 
-  if (activeRide.loading) {
-    return <Loading />;
-  }
+    const [startRide, startRideMutation] = useMutation(StartRideMutation, {
+        refetchQueries: [
+            {
+                query: ActiveRideQuery,
+                variables: {
+                    userId: props.userId,
+                },
+            }
+        ]
+    });
 
-  const isRideActive = Boolean(activeRide.data.activeRide);
+    const [endRide, endRideMutation] = useMutation(EndRideMutation, {
+        refetchQueries: [
+            {
+                query: ActiveRideQuery,
+                variables: {
+                    userId: props.userId,
+                },
+            }
+        ]
+    });
 
-  const handleStartStop = () => {
-    const activeRideData = activeRide.data.activeRide;
-    console.log(activeRideData);
-  };
+    if (activeRide.loading) {
+        return <Loading/>;
+    }
 
-  const getTitle = () => {
-    return isRideActive ? "Zakończ przejazd" : "Rozpocznij przejazd";
-  };
+    const isRideActive = Boolean(activeRide.data.activeRide);
 
-  return (
-    <div className="max-w-7xl mx-4 space-y-4 py-4 sm:px-8 lg:px-8">
-      <button
-        className="btn-primary active:btn-secondary font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => handleStartStop()}
-      >
-        {getTitle()}
-      </button>
-    </div>
-  );
+    const handleStartStop = () => {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            if (isRideActive) {
+                await startRide({
+                    variables: {
+                        userId: props.userId,
+                        start_lat: position.coords.latitude,
+                        start_lng: position.coords.longitude,
+                    },
+                });
+            } else {
+                await endRide({
+                    variables: {
+                        userId: props.userId,
+                        rideId: activeRide.data.activeRide.id,
+                        start_lat: position.coords.latitude,
+                        start_lng: position.coords.longitude,
+                    },
+                })
+            }
+        })
+    };
+
+    const getTitle = () => {
+        return isRideActive ? "Zakończ przejazd" : "Rozpocznij przejazd";
+    };
+
+    return (
+        <div className="max-w-7xl mx-4 space-y-4 py-4 sm:px-8 lg:px-8">
+            <button
+                className="btn-primary active:btn-secondary font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => handleStartStop()}
+            >
+                {getTitle()}
+            </button>
+        </div>
+    );
 }
---
+++ services/external/PointsService.ts
@@ -0,0 +1,25 @@
+import {particulateMatter10Status, PollutionStatus} from "./WaqiApi";
+
+const getPointsMultiplier = (pm10: number): number => {
+    const pm10Status = particulateMatter10Status(pm10)
+    switch (pm10Status) {
+        case PollutionStatus.Good:
+            return 1
+        case PollutionStatus.Fair:
+            return 1.5
+        case PollutionStatus.Poor:
+            return 2
+        case PollutionStatus.VeryPoor:
+            return 2.5
+        case PollutionStatus.ExtremelyPoor:
+            return 3
+        default:
+            return 1
+    }
+}
+
+export const calculatePoints = (distance: number, particulateMatter10: number): number => {
+    const multiplier = getPointsMultiplier(particulateMatter10);
+    console.log(distance)
+    return (distance / 1000) * multiplier * 0.05;
+};
