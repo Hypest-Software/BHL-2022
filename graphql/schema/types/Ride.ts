bb8e0b1be681797e572c587599a3d7de84adae45
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2022-04-09 07:31:46 +0200

    WIP

--- graphql/schema/types/Ride.ts
+++ graphql/schema/types/Ride.ts
@@ -6,102 +6,102 @@ import {
   nonNull,
   objectType,
   stringArg,
-} from "nexus";
-import { getPollution } from "../../../services/external/WaqiApi";
-import { getTransitInfo } from "../../../services/external/GoogleMapsAPI";
-import { calculatePoints } from "../../../services/external/PointsService";
+} from 'nexus'
+import { getPollution } from '../../../services/external/WaqiApi'
+import { getTransitInfo } from '../../../services/external/GoogleMapsAPI'
+import { calculatePoints } from '../../../services/external/PointsService'
 
 const Conveyance = enumType({
-  name: "Conveyance",
-  members: ["ELECTRIC_CAR", "TRANSIT", "BIKE"],
-});
+  name: 'Conveyance',
+  members: ['ELECTRIC_CAR', 'TRANSIT', 'BIKE'],
+})
 
 export const Ride = objectType({
-  name: "Ride",
+  name: 'Ride',
   definition(t) {
-    t.string("id");
-    t.float("start_lat");
-    t.float("start_lng");
-    t.float("end_lat");
-    t.float("end_lng");
-    t.float("distance");
+    t.string('id')
+    t.float('start_lat')
+    t.float('start_lng')
+    t.float('end_lat')
+    t.float('end_lng')
+    t.float('distance')
     // @ts-ignore
-    t.date("start_time");
+    t.date('start_time')
     // @ts-ignore
-    t.date("end_time");
-    t.string("conveyance");
-    t.float("points");
-    t.boolean("single_ride_ticket");
+    t.date('end_time')
+    t.string('conveyance')
+    t.float('points')
+    t.boolean('single_ride_ticket')
 
-    t.float("air_co");
-    t.float("air_no");
-    t.float("air_no2");
-    t.float("air_o3");
-    t.float("air_so2");
-    t.float("air_pm2_5");
-    t.float("air_pm10");
-    t.float("air_nh3");
+    t.float('air_co')
+    t.float('air_no')
+    t.float('air_no2')
+    t.float('air_o3')
+    t.float('air_so2')
+    t.float('air_pm2_5')
+    t.float('air_pm10')
+    t.float('air_nh3')
   },
-});
+})
 
 export const RideQueries = extendType({
-  type: "Query",
+  type: 'Query',
   definition: (t) => {
-    t.field("ride", {
-      type: "Ride",
+    t.field('ride', {
+      type: 'Ride',
       args: {
         rideId: nonNull(stringArg()),
       },
       resolve: (_, args, ctx) => {
         return ctx.prisma.ride.findUnique({
           where: { id: args.rideId },
-        });
+        })
       },
-    });
+    })
 
-    t.field("activeRide", {
-      type: "Ride",
+    t.field('activeRide', {
+      type: 'Ride',
       args: {
         userId: nonNull(stringArg()),
       },
       resolve: async (_, args, ctx) => {
         const latestRide = await ctx.prisma.ride.findFirst({
           where: { userId: args.userId },
-          orderBy: { start_time: "desc" },
-        });
+          orderBy: { start_time: 'desc' },
+        })
 
         if (!latestRide) {
-          return null;
+          return null
         }
 
         if (!latestRide.end_time) {
-          return latestRide;
+          return latestRide
         } else {
-          return null;
+          return null
         }
       },
-    });
+    })
 
-    t.list.field("rides", {
-      type: "Ride",
+    t.list.field('rides', {
+      type: 'Ride',
       args: {
         userId: nonNull(stringArg()),
       },
       resolve: (_, args, ctx) => {
         return ctx.prisma.ride.findMany({
           where: { userId: args.userId },
-          orderBy: { start_time: "asc" },
-        });
+          orderBy: { start_time: 'asc' },
+        })
       },
-    });
+    })
   },
-});
+})
 
 export const RideMutations = extendType({
-  type: "Mutation",
+  type: 'Mutation',
   definition: (t) => {
-    t.field("startRide", {
-      type: "Ride",
+    t.field('startRide', {
+      type: 'Ride',
       args: {
         userId: nonNull(stringArg()),
         start_lat: nonNull(floatArg()),
@@ -109,7 +109,7 @@ export const RideMutations = extendType({
         conveyance: nonNull(arg({ type: Conveyance })),
       },
       resolve: async (_, { userId, start_lat, start_lng, conveyance }, ctx) => {
-        const pollution = await getPollution();
+        const pollution = await getPollution()
         return ctx.prisma.ride.create({
           data: {
             userId,
@@ -121,12 +121,12 @@ export const RideMutations = extendType({
             particulateMatter25: pollution.particulateMatter25,
             particulateMatter10: pollution.particulateMatter10,
           },
-        });
+        })
       },
-    });
+    })
 
-    t.field("endRide", {
-      type: "Ride",
+    t.field('endRide', {
+      type: 'Ride',
       args: {
         rideId: nonNull(stringArg()),
         userId: nonNull(stringArg()),
@@ -136,18 +136,30 @@ export const RideMutations = extendType({
       resolve: async (_, { rideId, userId, end_lat, end_lng }, ctx) => {
         const ride = await ctx.prisma.ride.findUnique({
           where: { id: rideId },
-        });
+        })
 
         const transitInfo = await getTransitInfo(
           `${ride.start_lat},${ride.start_lng}`,
           `${end_lat},${end_lng}`,
-          "transit"
-        );
+          'transit'
+        )
 
         const points = calculatePoints(
           transitInfo.distance,
           ride.particulateMatter10
-        );
+        )
+
+        ctx.prisma.user.update({
+          where: { id: userId },
+          data: {
+            balance:
+              (
+                await ctx.prisma.user.findUnique({
+                  where: { id: userId },
+                })
+              ).balance + points,
+          },
+        })
 
         return ctx.prisma.ride.update({
           where: { id: rideId },
@@ -158,8 +170,8 @@ export const RideMutations = extendType({
             distance: transitInfo.distance,
             points,
           },
-        });
+        })
       },
-    });
+    })
   },
-});
+})
