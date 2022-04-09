fa3c48c5d22407dac578b0bd9952b885cca4e663
Piotrek Brzeziński <thewildtree@outlook.com>
2022-04-09 13:01:07 +0200

    Add entry screen

--- components/NotAuthorised.tsx
+++ components/NotAuthorised.tsx
@@ -3,23 +3,22 @@ import Image from 'next/image'
 
 export default function NotAuthorised(props) {
   return (
-    <main className="bg-gray-200 min-h-screen">
+    <main className="relative bg-gray-200 min-h-screen">
 
-      <div className="bg-gray-100 max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 border-t border-gray-200">
+      <div className=" max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 border-t border-gray-200">
         <div className="layout">
-          <div className="flex justify-center mt-8 text-center">
+          <div className="flex flex-col justify-center mt-4 text-center z-10">
             <div>
               <Image src={'/bus.png'} width={300} height={187} />
             </div>
-
-          </div>
-          <div className="flex-auto">
-
-            <div className="text-lg mb-2">You are not logged in!</div>
-            <button className="btn-green" onClick={() => signIn()}>
-              Sign in
+            <div className="text-xl mb-6">Witaj w naszej aplikacji!</div>
+            <button className="btn btn-green self-center" onClick={() => signIn()}>
+              Zaloguj się
             </button>
           </div>
+        </div>
+        <div className="absolute top-0 left-4 z-1">
+          <div className="p-5 bg-blue-300 w-80 h-24 rounded"></div>
         </div>
       </div>
     </main>
